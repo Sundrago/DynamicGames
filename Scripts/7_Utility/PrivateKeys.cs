cab7517de28998237fae896c819d36751cbed703
SunDrargon <88926252+Sundrago@users.noreply.github.com>
2024-04-15 12:25:03 +0900

    .gitignore updated and script meta files added
    This commit adds multiple picture texture meta files to the Assets/Docs directory. It also introduces meta files for several scripts located in the Assets/Scripts folder. The additions contribute to enhancing the graphics system and improving script management in the project.

---
+++ Scripts/7_Utility/PrivateKeys.cs
@@ -0,0 +1,9 @@
+namespace DynamicGames.Utility
+{
+    internal static class PrivateKey
+    {
+        public const string IronSourceAppKey = "1c563f2d5";
+        public const string AppstoreURL = "https://apps.apple.com/us/app/dynamic-games-games-on-island/id6443782791";
+        public const string PrivateServerURL = "https://data.sundragon.net/dynamic_games_data.csv";
+    }
+}
