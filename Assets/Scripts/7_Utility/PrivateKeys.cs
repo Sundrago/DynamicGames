319d2dde1e086b6a35d4ce74b745167d1102abf4
SunDrargon <88926252+Sundrago@users.noreply.github.com>
2024-04-22 00:15:22 +0900

    .gitignore updates

---
+++ Assets/Scripts/7_Utility/PrivateKeys.cs
@@ -0,0 +1,9 @@
+namespace DynamicGames.Utility
+{
+    internal static class PrivateKey
+    {
+        public const string IronSourceAppKey = "";
+        public const string AppstoreURL = "";
+        public const string PrivateServerURL = "";
+    }
+}
